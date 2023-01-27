7e09af4dbcc91e3edf09f82895c22067aa48666b
ascuadrado <ascuadrado@gmail.com>
2023-01-27 12:52:15 +0100

    Added medals to all

--- team-section.php
+++ team-section.php
@@ -14,28 +14,30 @@ $members = array_merge($obj->SEUL, $obj->TTU, $obj->Other);
 
     <div class="row">
       <?php
-      foreach ($members as $key => $value) {
+      foreach ($obj->TTU as $key => $value) {
         $img = ($value->verified)?('<img src="assets/img/badge.png" width=15% style="position:absolute;top:0px;left:0px">'):("");
-        echo '<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-          <div class="member" data-aos="fade-up" data-aos-delay="100">
-            <div class="member-img">
-              <img src="assets/img/team/'.$value->foto.'" class="img-fluid" alt="">
-              <div class="social">
-                <a href=""><i class="bi bi-twitter"></i></a>
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-            <div class="member-info">
-              <h4>'.$value->nombre.'</h4>
-              <span>'.$value->puesto.'</span>
-              '. $img .'
-            </div>
-          </div>
+                  # Medals 🥇🥈🥉🏓🔥🚙
+        $medals = str_repeat("🥇", $value->cat1);
+        $medals .= str_repeat("🥈", $value->cat2);
+        $medals .= str_repeat("🥉", $value->cat3);
+        $medals .= str_repeat("🏓", $value->balls);
+        $medals .= str_repeat("🔥", $value->fire);
+        $medals .= str_repeat("🚙", $value->coche);
+        echo '<div class="col-lg-6 col-md-6 col-sm-12 d-flex align-items-stretch">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+        <div class="member-img">
+        <img src="assets/img/team/'.$value->foto.'" class="img-fluid" alt="">
+        </div>
+        <div class="member-info">
+        <h4>'.$value->nombre.'</h4>
+        <span>'.$value->puesto.'</span>
+        '. $img .'
+        <span>'. $medals .'</span>
+        </div>
+        </div>
         </div>';
       }
-       ?>
+      ?>
     </div>
 
   </div>
